e096b639e76e277690125ba5800c74a6102b6d3c
Rafik567 <rafmatevosyan@mail.ru>
2024-12-30 15:16:55 +0400

    new header & dishes

---
+++ src/Components/Header/HeaderTop.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import TopEl from './TopEl'
+
+const HeaderTop = () => {
+    return (
+        <>
+            <div className='flex justify-center pt-[10px] gap-[50px]'>
+                <div className='flex gap-[30px]'>
+                    <img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAYFBMVEX///8AAADPz89LS0uWlpb39/eCgoKQkJCxsbH29vZiYmI4ODh0dHTX19empqbFxcXr6+sQEBDh4eEbGxu7u7s0NDR6enpXV1egoKDJyclvb28ODg6IiIhcXFwfHx8ZGRnwNjATAAACZUlEQVR4nO3dCW7CMBCFYRdIw75vbSm9/y2rqKgUVRo72NJoxv93gveUkGBj7BAAAAAAAAAAAAAAAAAAoAKrdjq0Y9qu+tVbH1/sOa7TC7baYZ/UJvZrZtpJnzZrkgputHNm2KRUPGinzHKIF3zVzpjpNVZwq50w2zbScKodMNtULjjRzlfARGw41o5XwFhsONeOV8BcbGj3ZX83Extqpyui8oY77XQFXMWGJ+14BZzEhlbHTX/JY6iBdrwCFmJDD48auWBYaufLtow0NP803cUKhoV2xEyRT6H9+zR6j3bO2ikznFMKhrDSzvm05GnhxuYgap40l3izHlmbcpuNekx53y7kdmDHts/lAwAAAAAAAAAAxjRvy5Edy7e+P1zsh9q/JfU23PfoN7hqx33KdZBa0O5i9ugy9h+f2jkzfKYUfNdOmeU9XtD6Sm95lXfwsFhfXqofwkU7YLZLpKF2vgLkgnYXC93Jy4bsvgrv5JeivS9r/w3Fhh/a8QrYiA210xVR+TX0/zn0/yz1/z708KiRC1bwvdT+2CI6JeV+fFjBGL+CeRrLT5vEubYK5kuD/znvjvffLQAAAAAAAAAAgCHO94myt9fXoddeXxOj+7XFFkD/srtsKHHPPff7Jrrf+9L//qVf2hEzRfegtX2PdmL3qXa+AuSC/vfz9r8nu/999a3v5t2Rn6ba6YqovKH/
+                    // c2ZsDpseyWcFWV/l3ZFXettfqh/9I7D7c9cqODvP/H+7EhazW5tke5RwhmVoLI+Bk84h9X+WbLA7hko9DzhUcKZzx
+                    // /m53AAAAAAAAAAAAAAAAABg0zfn21Nf0tdOJAAAAABJRU5ErkJggg==' className='w-[50px] h-[50px]' />
+                    <img src='https://buy.am/logo.svg' />
+                </div>
+                <div className='flex gap-[80px] pt-[15px]'>
+                    <TopEl text={"Նախաճաշ"}/>
+                    <TopEl text={"Աղցաներ"}/>
+                    <TopEl text={"Սառը նախուտեստներ"}/>
+                    <TopEl text={"Տաք նախուտեստներ"}/>
+                    <TopEl text={"Հիմանական ուտեստներ"}/>
+                    <TopEl text={"Աղանդներ"}/>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HeaderTop
---
+++ src/Components/Header/TopEl.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TopEl = ({ text }) => {
+    return (
+        <>
+            <div>
+                <p className='text-gray-500'>{text}</p>
+            </div>
+        </>
+    )
+}
+
+export default TopEl
